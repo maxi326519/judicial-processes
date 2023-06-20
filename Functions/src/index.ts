5ee15a5482f5fb0be12568eb69aef91ac433044c
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2023-06-20 11:41:48 -0300

    Update emulator, and functions for user table

--- Functions/src/index.ts
+++ Functions/src/index.ts
@@ -1,9 +1,115 @@
-import * as functions from "firebase-functions";
+import { HttpsError, onCall } from "firebase-functions/v1/https";
+import admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.setNewUser = onCall(async (data, context) => {
+  try {
+    // Validate user admin
+    const userUid = context.auth?.token.uid;
+    const firestore = admin.firestore();
+    if (!userUid)
+      throw new HttpsError("unauthenticated", "user unauthenticated");
+    const requestUser = await firestore.collection("Users").doc(userUid).get();
+    if (!requestUser.exists)
+      throw new HttpsError("not-found", "user not found");
+    if (requestUser.data()!.rol !== "Admin")
+      throw new HttpsError("permission-denied", "must have admin rol");
+
+    // Validate parameters
+    const { rol, name, email, password } = data;
+    if (!rol || !name || !email || !password)
+      throw new HttpsError("invalid-argument", "missing parameters");
+
+    // Create user
+    const newUser = await admin.auth().createUser({ email, password });
+
+    // Add user to database
+    await firestore
+      .collection("Users")
+      .doc(newUser.uid)
+      .set({ rol, name, email });
+
+    return {
+      uid: newUser.uid,
+    };
+  } catch (error) {
+    throw new HttpsError("internal", "Error to create user");
+  }
+});
+
+exports.updateUser = onCall(async (data, context) => {
+  try {
+    // Validate user admin
+    const userUid = context.auth?.token.uid;
+    const firestore = admin.firestore();
+    if (!userUid)
+      throw new HttpsError("unauthenticated", "user unauthenticated");
+    const requestUser = await firestore.collection("Users").doc(userUid).get();
+    if (!requestUser.exists)
+      throw new HttpsError("not-found", "user not found");
+    if (requestUser.data()!.rol !== "Admin")
+      throw new HttpsError("permission-denied", "must have admin rol");
+
+    // Validate parameters
+    const { id, rol, name, email, password } = data;
+    if (!id || !rol || !name || !email || !password)
+      throw new HttpsError("invalid-argument", "missing parameters");
+
+    // Update user
+    const newUser = await admin
+      .auth()
+      .updateUser(id, { email, password })
+      .catch(() => {
+        throw new HttpsError("internal", "Error to update user");
+      });
+
+    // Update user to database
+    await firestore
+      .collection("Users")
+      .doc(newUser.uid)
+      .update({ rol, name, email })
+      .catch(() => {
+        throw new HttpsError("internal", "Error to update user to database");
+      });
+  } catch (error) {
+    throw new HttpsError("internal", "Error to update user");
+  }
+});
+
+exports.deleteUser = onCall(async (data, context) => {
+  try {
+    // Validate user admin
+    const userUid = context.auth?.token.uid;
+    const firestore = admin.firestore();
+    if (!userUid)
+      throw new HttpsError("unauthenticated", "user unauthenticated");
+    const requestUser = await firestore.collection("Users").doc(userUid).get();
+    if (!requestUser.exists)
+      throw new HttpsError("not-found", "user not found");
+    if (requestUser.data()!.rol !== "Admin")
+      throw new HttpsError("permission-denied", "must have admin rol");
+
+    // Validate parameters
+    const uid = data.uid;
+    if (!uid) throw new HttpsError("invalid-argument", "missing parameters");
+
+    // Delete user
+    await admin
+      .auth()
+      .deleteUser(uid)
+      .catch(() => {
+        throw new HttpsError("internal", "Error to delete user");
+      });
+
+    // Delete user to database
+    await firestore
+      .collection("Users")
+      .doc(uid)
+      .delete()
+      .catch(() => {
+        throw new HttpsError("internal", "Error to delete user to database");
+      });
+  } catch (error: any) {
+    throw new HttpsError("internal", error.message);
+  }
+});
